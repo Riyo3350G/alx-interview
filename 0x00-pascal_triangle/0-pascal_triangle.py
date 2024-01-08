f4aefc023b49c9296013e6b7c99550569f6eaef3
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2024-01-08 07:31:50 -0800

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-
+"""
+pascal triangle module
+"""
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
+    '''Function that returns Pascal triangle of n'''
     triangle = []
-
-    for i in range(n):
-        row = [1] * (i + 1)
-
-        for j in range(1, len(row) - 1):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-
-        triangle.append(row)
-
-    return triangle
+    if n <= 0:
+        return triangle
+    triangle = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(triangle[i - 1]) - 1):
+            current = triangle[i - 1]
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        temp.append(1)
+        triangle.append(temp)
+    return k
