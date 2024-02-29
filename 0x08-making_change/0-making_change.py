3f7d12243a49f7e03313121d70c40609262d1125
riyo3350g <yechouben@gamail.com>
2024-02-29 18:43:38 +0100

    Add Task 0 Req Fix 1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,17 +6,16 @@ def makeChange(coins, total):
     """Function that determines the fewest number of
     coins needed to meet a given amount total.
     If the total cannot be met, return -1."""
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    # Initialize an array to store the minimum number of coins for each amount
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed to make change for 0
-
-    # Iterate through each coin value
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        # Update dp array for each amount from coin value to total
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
